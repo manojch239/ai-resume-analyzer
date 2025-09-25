7852f84e1487e642210050d02c48852bced22780
manojm <manojch239@gmail.com>
2025-09-25 17:08:33 +0530

    Authentication

---
+++ app/routes/auth.tsx
@@ -0,0 +1,64 @@
+import { sep } from 'path';
+import React, { use, useEffect } from 'react'
+import { useLocation, useNavigate, type NavigateFunction } from 'react-router';
+import { usePuterStore } from '~/lib/puter';
+
+
+
+export const metadata = {
+  title: 'Resumind | Auth',
+  name : 'description',
+  description: 'Authentication Page',
+  content : 'Login to your account'
+}
+
+const auth = () => {
+    const { isLoading, auth } = usePuterStore();
+    const location = useLocation();
+    const next: string = location.search.split('next=')[1];
+    const navigate: NavigateFunction = useNavigate();
+
+    useEffect(() => {
+        if (auth.isAuthenticated) {
+            // Redirect to home or dashboard after successful login
+            navigate(next);
+            // window.location.href = '/';
+        }
+    }, [auth.isAuthenticated, next]);
+
+    return (
+        <main className="bg-[url('/images/bg-main.svg')] bg-cover min-h-screen flex items-center justify-center">
+            <div className='gradient-border shadow-lg'>
+                <section className='flex flex-col gap-8 bg-white rounded-2xl p-10'>
+                    <div className='flex flex-col items-center gap-2 text-center'>
+                    <h1>Welcome</h1>
+                    <h2>Log in to Continue your job Journey</h2>
+                </div>
+                <div>
+                    {isLoading? (
+                        <button className='auth-button animate-pulse'>
+                            <p>Signing you in ...</p>
+                        </button>
+                    ) : (
+                        <>
+                            {auth.isAuthenticated ? (
+                                <button className='auth-button' onClick = {auth.signOut}>
+                                    <p>Log Out</p>
+                                    </button>
+                            ) : (
+                                <button className='auth-button' onClick={auth.signIn}>
+                                    <p>Log in</p>
+                                </button>
+                            )}
+                        </>
+                    )}
+                </div>
+            </section>
+
+        </div>
+
+    </main>
+  )
+}
+
+export default auth
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,52 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import type Navbar from "~/components/Navbar";
+import { resumes } from "../../constants";
+import { callbackify } from "util";
+import ResumeCard from "~/components/ResumeCard";
+import { useLocation, useNavigate, type NavigateFunction } from 'react-router';
+import { usePuterStore } from '~/lib/puter';
+import { useEffect } from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+   const { isLoading, auth } = usePuterStore();
+    const location = useLocation();
+    const next: string = location.search.split('next=')[1];
+    const navigate: NavigateFunction = useNavigate();
+
+    useEffect(() => {
+        if (!auth.isAuthenticated) {
+            // Redirect to home or dashboard after successful login
+            navigate('auth?next=/');
+            // window.location.href = '/';
+        }
+    }, [auth.isAuthenticated]);
+
+
+
+
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+
+    <section className="main-section">
+      <div className="page-heading py-16">
+         <h1>Track Your Application & Resume Ratings</h1>
+         <h2>Review your submission and check AI powered feedback.</h2>
+      </div>
+    </section>
+
+    {resumes.length > 0 && (
+    <div className="resumes-section">
+      {resumes.map((resume) => (
+        <ResumeCard key={resume.id} resume ={resume} />
+      ))}
+    </div>
+    )}
+  </main>
 }
