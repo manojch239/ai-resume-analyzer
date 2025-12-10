6441bb066e40d74608e3b08df57e945407d44b9a
manojm <manojch239@gmail.com>
2025-12-10 20:57:36 +0530

    cloud storage,pdf2img conv, Key-Value storage and AI feedback

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -3,7 +3,7 @@ import Navbar from "~/components/Navbar";
 import FileUploader from "~/components/FileUploader";
 import {usePuterStore} from "~/lib/puter";
 import {useNavigate} from "react-router";
-// import {convertPdfToImage} from "~/lib/pdf2img";
+import {convertPdfToImage} from "~/lib/pdf2img";
 import {generateUUID} from "~/lib/utils";
 import {prepareInstructions} from "../../constants";
 
@@ -48,7 +48,7 @@ const Upload = () => {
 
         const feedback = await ai.feedback(
             uploadedFile.path,
-            prepareInstructions({ jobTitle, jobDescription })
+            prepareInstructions({ jobTitle, jobDescription})
         )
         if (!feedback) return setStatusText('Error: Failed to analyze resume');
 
